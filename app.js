f565710e58f0a1181cba9cf22faa6c7fe289d502
ahmedhussin1 <ahemd208087@gmail.com>
2022-08-23 14:55:05 +0200

    add to wishlist btn working

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let totalPrice=document.getElementById("total-price");//total price of items
 let prices = document.querySelectorAll(".itm-price")//get each item price
 let element= document.querySelectorAll(".cart-ele2");
 let trash = document.querySelectorAll(".fa-trash-can");
+let love = document.querySelectorAll(".fa-heart");
+let noItm=document.getElementById("noOfItems");
 
 // increasing elements counter----------------------
 Plus.forEach((btn, idx) => {
@@ -14,6 +16,7 @@ Plus.forEach((btn, idx) => {
     btn.addEventListener("click", add = () => {
         plusCounter.innerHTML = parseInt(plusCounter.innerHTML) + 1;//increase the counter by one 
         totalPrice.innerHTML = parseInt(totalPrice.innerHTML)+parseInt(itmCounter.innerHTML);//
+        noItm.innerHTML=parseInt(noItm.innerHTML)+1;
     })
 })
 // decreasing elements counter-----------------------
@@ -25,13 +28,25 @@ minus.forEach((btn,idx)=>{
         if(parseInt(minusCounter.innerHTML)>0){
             minusCounter.innerHTML=parseInt(minusCounter.innerHTML)-1;
             totalPrice.innerHTML = parseInt(totalPrice.innerHTML)-parseInt(itmCounter.innerHTML);
+            noItm.innerHTML=parseInt(noItm.innerHTML)-1;
         }
     })
 })
-
+//remove element from the cart
 trash.forEach((btn,idx)=>{
     let ele=element[idx];
     btn.addEventListener("click",add=()=>{
         ele.remove();
+    })
+})
+//add element to wishlist 
+love.forEach((btn,idx)=>{
+    let loveCounter = love[idx];
+    btn.addEventListener("click",onclick=()=>{
+        if(loveCounter.style.color == "red"){
+            loveCounter.style.color = "black"
+        }else{
+            loveCounter.style.color = "red"
+        }
     })
 })
