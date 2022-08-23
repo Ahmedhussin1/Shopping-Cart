5c3fbaebf6117711ea2d5204ab0b988bd00537cc
ahmedhussin1 <ahemd208087@gmail.com>
2022-08-23 14:31:52 +0200

    remove btn working

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ let minus = document.querySelectorAll(".fa-minus")//get all minus icons
 let priceTag = document.querySelectorAll(".priceTag")//no. of items selected
 let totalPrice=document.getElementById("total-price");//total price of items
 let prices = document.querySelectorAll(".itm-price")//get each item price
+let element= document.querySelectorAll(".cart-ele2");
+let trash = document.querySelectorAll(".fa-trash-can");
 
-// increasing elements counter
+// increasing elements counter----------------------
 Plus.forEach((btn, idx) => {
     let plusCounter = priceTag[idx];
     let itmCounter=prices[idx];
@@ -14,39 +16,22 @@ Plus.forEach((btn, idx) => {
         totalPrice.innerHTML = parseInt(totalPrice.innerHTML)+parseInt(itmCounter.innerHTML);//
     })
 })
-// decreasing elements counter
+// decreasing elements counter-----------------------
 minus.forEach((btn,idx)=>{
     let minusCounter=priceTag[idx];
     let itmCounter=prices[idx];
+    let ele=element[idx];
     btn.addEventListener("click",add=()=>{
         if(parseInt(minusCounter.innerHTML)>0){
             minusCounter.innerHTML=parseInt(minusCounter.innerHTML)-1;
             totalPrice.innerHTML = parseInt(totalPrice.innerHTML)-parseInt(itmCounter.innerHTML);
-
         }
     })
 })
 
-let loveBtn = document.querySelectorAll(".move-wishlist")
-let loveCount=1
-loveBtn.forEach((btn,idx)=>{
-    loveCount++;
-    btn.addEventListener("click",love=()=>{
-        if(loveCount%2==0){
-            loveBtn.style.color='red';
-        }
-        else{
-            loveBtn.style.color='black';
-        }
+trash.forEach((btn,idx)=>{
+    let ele=element[idx];
+    btn.addEventListener("click",add=()=>{
+        ele.remove();
     })
 })
-// document.getElementById("love").onclick=function(){
-//     loveCount++;
-//     if(loveCount%2==0){
-//         loveBtn.style.color='red';
-//     }
-//     else{
-//         loveBtn.style.color='black';
-//     }
-//     console.log('red');
-// }
